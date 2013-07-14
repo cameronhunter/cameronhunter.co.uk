5f1663df14f9825cd62c4e9edbf199256c4199c3
Cameron Hunter <hello@cameronhunter.co.uk>
2013-07-14 13:21:13 -0700

    Adding google analytics script

---
+++ src/js/google-analytics.js
@@ -0,0 +1,6 @@
+var _gaq = [['_setAccount', 'UA-1802050-8'], ['_trackPageview'], ['_trackPageLoadTime']];
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
